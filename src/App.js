bdd774bf40c766ec4d0efc3a5b272f702f523676
RomainRoy83 <romain.roy.83@gmail.com>
2022-07-15 19:16:15 +0200

    Functional app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+import './App.css'
 
 function App() {
+  const [simpsons, setSimpsons] = useState(' ')
+
+  useEffect(() => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then(res => setSimpsons(res.data[0]))
+  }, []);
+
+  const handleClick = (() => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then(res => setSimpsons(res.data[0]))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Simpsons API</h1>
+
+          <div className={simpsons.characterDirection === 'Right' ? 'content' : 'content reverse'} id="simpsonsContent">
+            {console.log(simpsons)}
+            <div className="charInfo">
+              <h1 className="charName">{simpsons.character}</h1>
+              <p className="quote">{simpsons.quote}</p>
+            </div>
+            <div className="imageContainer">
+              <img src={simpsons.image} className="image" alt={simpsons.character} />
+            </div>
+          </div>
+
+          <button className="generateQuote" onClick={handleClick}>One more!</button>
+        </div>
+      </section>
     </div>
   );
 }
